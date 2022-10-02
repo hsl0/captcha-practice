ed7e722eda24ecd38cb85fe5be8b6daefb136566
hsl0 <hsl0@outlook.kr>
2022-10-03 00:41:13 +0900

    Init 2

---
+++ main.ts
@@ -0,0 +1,60 @@
+const answer = document.getElementById('answer') as HTMLPreElement;
+const input = document.getElementById('input') as HTMLInputElement;
+const main = document.getElementsByTagName('main')[0];
+const form = document.querySelector('main form') as HTMLFormElement;
+
+// 출처: https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Math/random#%EC%B5%9C%EB%8C%93%EA%B0%92%EC%9D%84_%ED%8F%AC%ED%95%A8%ED%95%98%EB%8A%94_%EC%A0%95%EC%88%98_%EB%82%9C%EC%88%98_%EC%83%9D%EC%84%B1%ED%95%98%EA%B8%B0
+function getRandomNumber(max: number): number;
+function getRandomNumber(min: number, max: number): number;
+function getRandomNumber(min: number, max?: number): number {
+    if (!max) {
+        max = min;
+        min = 0;
+    }
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomChar() {
+    return getRandomNumber(35).toString(36).toUpperCase();
+}
+
+function getRandomString(length: number = 1): string {
+    let str = '';
+
+    while (str.length < length) {
+        str += getRandomChar();
+    }
+
+    return str;
+}
+
+function changeAnswer() {
+    answer.innerText = getRandomString(6);
+    input.value = '';
+    main.classList.remove('wrong');
+}
+
+changeAnswer();
+
+input.addEventListener('input', (event) => {
+    const target = event.currentTarget as HTMLInputElement;
+    if (!target.value[target.value.length - 1].match(/[0-9a-z]/i))
+        target.value = target.value.slice(0, -1);
+    target.value = target.value.toUpperCase();
+});
+form.addEventListener(
+    'submit',
+    (event) => {
+        event.preventDefault();
+        if (answer.innerText === input.value.toUpperCase()) changeAnswer();
+        else main.classList.add('wrong');
+        input.focus();
+    },
+    true
+);
+form.addEventListener('reset', (event) => {
+    changeAnswer();
+    input.focus();
+});
