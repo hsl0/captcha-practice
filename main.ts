9979e461aa998a547562d1e2933df9a8f8e92fff
hsl0 <hsl0@outlook.kr>
2022-11-20 02:54:49 +0900

    키워드 추가, 폰트 변경, 한글 연속 입력시 앞글자가 지워지는 버그 수정

--- main.ts
+++ main.ts
@@ -2,6 +2,8 @@ const answer = document.getElementById('answer') as HTMLPreElement;
 const input = document.getElementById('input') as HTMLInputElement;
 const main = document.getElementsByTagName('main')[0];
 const form = document.querySelector('main form') as HTMLFormElement;
+let last: string = '';
+let backspace = false;
 
 // 출처: https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Math/random#%EC%B5%9C%EB%8C%93%EA%B0%92%EC%9D%84_%ED%8F%AC%ED%95%A8%ED%95%98%EB%8A%94_%EC%A0%95%EC%88%98_%EB%82%9C%EC%88%98_%EC%83%9D%EC%84%B1%ED%95%98%EA%B8%B0
 function getRandomNumber(max: number): number;
@@ -38,11 +40,24 @@ function changeAnswer() {
 
 changeAnswer();
 
+input.addEventListener('keydown', (event) => {
+    if (event.key === 'Backspace') backspace = true;
+});
+input.addEventListener('keyup', (event) => {
+    if (event.key === 'Backspace') backspace = false;
+});
 input.addEventListener('input', (event) => {
     const target = event.currentTarget as HTMLInputElement;
-    if (!target.value[target.value.length - 1].match(/[0-9a-z]/i))
-        target.value = target.value.slice(0, -1);
+    let start = target.selectionStart || 0;
+    if ((event as InputEvent).inputType.match(/delete/) && !backspace) {
+        target.value = last;
+        start++;
+    }
+    if (target.value.search(/[^0-9A-z]/) >= 0) start--;
+    target.value = target.value.replace(/[^0-9A-z]/g, '');
     target.value = target.value.toUpperCase();
+    last = target.value;
+    target.setSelectionRange(start, start);
 });
 form.addEventListener(
     'submit',
